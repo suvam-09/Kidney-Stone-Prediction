7b7f8893ae797b6bb4a6446232d9851679a43766
suvam-09 <suvam.kumar@yahoo.com>
2023-04-26 16:37:27 +0530

    commit changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np, pandas as pd
 import pickle, base64
 
 # load the trained model
-model = pickle.load(open('./models/trained_model.pkl', 'rb'))
+model = pickle.load(open('./models/random_forest_model.pkl', 'rb'))
 
 # define a function to make prediction
 def predict_target(gravity, ph, osmo, cond, urea, calc, osmo_cond_ratio, urea_calc_diff, osmo_urea_interaction, gravity_bin, ph_bin, osmo_bin, cond_bin, urea_bin, calc_bin):
@@ -47,13 +47,14 @@ def add_bg_img(image_file):
     """,
     unsafe_allow_html=True
         )
-        
+
 # add_bg_img('./Images/pexels-8325982.jpg')
 
 # add a title and a header
 st.markdown("<h1 style='text-align: center; color: black;'>Kidney Stone Prediction</h1>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: justify; color: black;'>The <i>six</i> physical characteristics of urine, <i>specific gravity</i>, <i>pH value</i>, <i>osmolarity</i>, <i>conductivity</i>, <i>urea</i> and <i>calcium  concentration</i> alongside few secondary characteristics have been provided below.</p>", unsafe_allow_html=True)
-st.markdown("<h5 style='text-align: center; color: black;'>Kindly drag the slider to enter the values and click on 'Predict' button</h5>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: justify; color: black;'>The <i>six</i> physical characteristics of urine, <i>specific gravity</i>, <i>pH value</i>, <i>osmolarity</i>, <i>conductivity</i>, <i>urea</i> and <i>calcium  concentration</i> alongside few secondary characteristics have been provided below. Kindly drag the slider to your left/right for entering the corresponding values based on your report and click on '<b>Predict</b>' button to see if your report indicates presence of any stone in your kidney.</p>", unsafe_allow_html=True)
+
+# st.markdown("<h5 style='text-align: center; color: black;'>Kindly drag the slider to enter the values and click on 'Predict' button</h5>", unsafe_allow_html=True)
 
 # add input fields for the features
 gravity = st.slider('specific gravity', min_value=float(data.gravity.min()), max_value=float(data.gravity.max()), value=float(data.gravity.mean()))
@@ -71,17 +72,20 @@ osmo_bin = st.slider("osmolarity_bin value", min_value=float(data.osmo_bin.min()
 cond_bin = st.slider("conductivity_bin value", min_value=float(data.cond_bin.min()), max_value=float(data.cond_bin.max()), value=float(data.cond_bin.mean()))
 urea_bin = st.slider("urea_bin value", min_value=float(data.urea_bin.min()), max_value=float(data.urea_bin.max()),value=float(data.urea_bin.mean()))
 calc_bin = st.slider("calc_bin value", min_value=float(data.calc_bin.min()), max_value=float(data.calc_bin.max()), value=float(data.calc_bin.mean()))
+# st.divider()
 
 # add a button to make the prediction
-if st.button("Predict"):
+col1, col2, col3, col4, col5 = st.columns(5)
+
+if col3.button("Predict"):
     prediction = predict_target(gravity, ph, osmo, cond, urea, calc, osmo_cond_ratio, urea_calc_diff, osmo_urea_interaction, gravity_bin, ph_bin, osmo_bin, cond_bin, urea_bin, calc_bin)
     st.write("Predicted Value: ", prediction)
     st.divider()
     if prediction > 0.3:
-        st.markdown("<h5 style='text-align: center; color: black;'><i>Report suggests presence of stones in your kidney</i></h5>", unsafe_allow_html=True)
+        st.markdown("<h5 style='text-align: center; color: black;'>Report suggests presence of stones in your kidney</h5>", unsafe_allow_html=True)
         st.markdown("<p style='text-align: center; color: black;'><i>Please do not panic. Consider visiting a urologist/nephrologist for further consultation.</i></p>", unsafe_allow_html=True)
         st.divider()
     else:
-        st.markdown("<h5 style='text-align: center; color: black;'><i>Report doesn't account for presence of stones in your kidney</i></h5>", unsafe_allow_html=True)
+        st.markdown("<h5 style='text-align: center; color: black;'>Report does not account for presence of stones in your kidney</h5>", unsafe_allow_html=True)
         st.markdown("<p style='text-align: center; color: black;'><i>Personal advise would be to visit a urologist/nephrologist for final confirmation.</i></p>", unsafe_allow_html=True)
         st.divider()
